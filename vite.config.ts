46d02684f2e2df500067eacbc939e8cf48c9c178
rawaddddd <rawad.elnemer@gmail.com>
2025-01-11 01:21:03 +0300

    chore: setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "tailwindcss"
+import autoprefixer from "autoprefixer"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    root: "./",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        }
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss(), autoprefixer()]
+        }
+    },
+    build: {
+        outDir: "dist",
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'index.min.js',
+                assetFileNames: 'styles.min.css',
+            }
+        }
+    },
 })
