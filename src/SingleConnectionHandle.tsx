297e534db0dd2956da3232652cf00c9f53e657d0
rawaddddd <rawad.elnemer@gmail.com>
2025-01-12 03:17:59 +0300

    fix: fix single connection handle allowing multiple connections

--- src/SingleConnectionHandle.tsx
+++ src/SingleConnectionHandle.tsx
@@ -3,6 +3,7 @@ import { Handle, HandleProps, useHandleConnections } from "@xyflow/react";
 function SingleConnectionHandle(props: HandleProps) {
   const connections = useHandleConnections({
     type: props.type,
+    id: props.id,
   });
 
   return <Handle {...props} isConnectable={connections.length === 0} />;
