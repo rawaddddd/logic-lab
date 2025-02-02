d072905f9e319dc4698bbc2b320e771c69f21978
rawaddddd <rawad.elnemer@gmail.com>
2025-02-02 09:42:41 +0300

    feat: add chip creation dialog menu

--- src/components/ChipCreationMenu.tsx
+++ src/components/ChipCreationMenu.tsx
@@ -2,32 +2,70 @@ import { useSimulationStore } from "@/store";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
 
 function ChipCreationMenu() {
   const createChip = useSimulationStore((state) => state.createChip);
   const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div className="px-4 py-2 flex flex-row items-center shadow-md rounded-md border bg-white space-x-2">
-        <Button onClick={() => createChip(name)}>Create chip</Button>
-        <Input
-          className="w-fit"
-          placeholder="Chip Name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      {/* <div className="shadow-md rounded-md">
-          <Button
-            className="h-full w-full"
-            size="lg"
-            onClick={() => createChip(name)}
-          >
-            Create chip
-          </Button>
-        </div> */}
-    </>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="h-full text-md">Create chip</Button>
+      </DialogTrigger>
+      <DialogPortal>
+        <form
+          onSubmit={(event) => {
+            console.log("here");
+            event.preventDefault();
+            createChip(name);
+            setOpen(false);
+          }}
+        >
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create chip</DialogTitle>
+              <DialogDescription>
+                Convert the current circuit into a reusable chip.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid grid-cols-[1fr,5fr] items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                name="name"
+                placeholder="Chip Name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                autoComplete="off"
+              />
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button type="submit">Save</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </DialogPortal>
+    </Dialog>
   );
 }
 
